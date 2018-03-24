e0015036144629f83f8b183db50123d6a1fca83c
RickyND <duongphu82@gmail.com>
2018-03-24 11:12:35 +0700

    recent query.

---
+++ dashboard/RecentQuery.sql
@@ -0,0 +1,20 @@
+SELECT TOP 10 
+qt.query_text_id,
+rs.avg_duration,
+qt.query_sql_text
+--q.query_id,
+--p.plan_id,
+--GETUTCDATE() AS CurrentUTCTime,
+--rs.last_execution_time   
+FROM sys.query_store_query_text AS qt   
+JOIN sys.query_store_query AS q   
+    ON qt.query_text_id = q.query_text_id   
+JOIN sys.query_store_plan AS p   
+    ON q.query_id = p.query_id   
+JOIN sys.query_store_runtime_stats AS rs   
+    ON p.plan_id = rs.plan_id  
+WHERE rs.last_execution_time > DATEADD(hour, -1, GETUTCDATE())  
+ORDER BY rs.avg_duration DESC;  
+
+-- select * from sys.query_store_query_text
+-- where query_text_id =552
